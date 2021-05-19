540e49062cdfdd1e481aeea819a1122585b0633a
Lee Ter Yi <lteryi@gmail.com>
2021-05-19 18:02:06 +0800

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ wss.on('connection', (ws) => {
   console.log('Client connected');
   ws.on('close', () => console.log('Client disconnected'));
   ws.on('message', message => {
-    
+      wss.clients.forEach((client) => {
+          client.send(message);
+      });
   })
 });
 
